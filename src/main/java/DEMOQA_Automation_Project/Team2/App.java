3306fba1f5c577e7afed73923c3d2c17f84a8a6b
Eng.Nisreen Elaian <nisreenhisham50@gmail.com>
2025-04-22 21:27:37 +0300

    Initial Maven project commit

---
+++ src/main/java/DEMOQA_Automation_Project/Team2/App.java
@@ -0,0 +1,7 @@
+package DEMOQA_Automation_Project.Team2;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/DEMOQA_Automation_Project/Team2/AppTest.java
@@ -0,0 +1,13 @@
+package DEMOQA_Automation_Project.Team2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
